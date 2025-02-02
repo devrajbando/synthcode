69112a59149949c526fb1b3c99749707cf76483c
Arman Bhattacharjee <66437768+quacky20@users.noreply.github.com>
2025-02-02 22:29:30 +0530

    Update tester2.py

--- mlbackend/tester2.py
+++ mlbackend/tester2.py
@@ -3,17 +3,12 @@ import requests
 API_URL = "http://127.0.0.1:8000/fixsyntax"
 
 data = {
-<<<<<<< HEAD
-    "codeSnippet": "def hello_world()\n    print('World')",
-    "maxLen": 100
-=======
     "codeSnippet": '''function greet(name) {
         console.log("Hello, " + name + "!");
 }
 
 greet("Alex");''',
-    "maxLen":  200
->>>>>>> e62fff1dffc0238e1e5185e19230f701e1ce2c0c
+    "maxLen": 200
 }
 
 response = requests.post(API_URL, json=data)
